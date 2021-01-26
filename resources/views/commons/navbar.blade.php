1c8ab6c068182149ffa98a0ce0e5c34422f82a58
shou.fukuhara <sho.fukuhara0213@gmail.com>
2021-01-26 15:17:13 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,8 +8,11 @@
         
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+            <ul class="nav navbar-nav navbar-right">
+                {{--ユーザーの登録--}}
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                {{--ログインページ--}}
+                <li><a href="#">Login</a></li>
             </ul>
         </div>
     </nav>
